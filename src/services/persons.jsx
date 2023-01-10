a9a5faed942d60285457e0ad95074939e1c04125
Ondairos <ondairos@gmail.com>
2023-01-10 13:43:25 +0200

    added correct proxy config code for getting data

--- src/services/persons.jsx
+++ src/services/persons.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const baseUrl = "http://localhost:3001/persons";
-const baseUrl = "/persons";
+// const baseUrl = "http://localhost:3001/api/persons";
+const baseUrl = "/api/persons";
 
 
 //get
